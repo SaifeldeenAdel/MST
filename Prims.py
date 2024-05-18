5a79b00852aca922f27eb53e79145d7b84f76f14
Saif <saifeldeenadel77@gmail.com>
2024-05-18 09:18:32 +0300

    Fixed prims and added sami's graph

--- Prims.py
+++ Prims.py
@@ -1,5 +1,6 @@
 from Graph import Graph
 import sys
+
 class Prims:
   def __init__(self) -> None:
     self.mst = {}
@@ -8,33 +9,45 @@ class Prims:
     V = graph.vertices
     edges = graph.graph
 
-    selectedNodes = [False] * V
+    selectedNodes = [0] * V
     selectedNodes[0] = True
     numberOfEdges = 0
 
-    while numberOfEdges < V -1:
+    while (numberOfEdges < V -1):
       mini = sys.maxsize
-      selected = 0
+      x = 0
+      y = 0
       for i in range(V):
         if selectedNodes[i]:
           for j in range(V):
             if ((not selectedNodes[j]) and edges[i][j]):
               if edges[i][j] < mini:
                 mini = edges[i][j]
-                self.mst[(i,j)]  = edges[i][j]
-                selected = j
+                x = i
+                y = j
 
-      selectedNodes[selected] = True
+      self.mst[(x,y)]  = edges[x][y]
+      selectedNodes[y] = True
       numberOfEdges += 1
 
 
   def printMST(self):
     print("Edge     :   Weight")
+    alphadict = {0: 'a', 1:'b', 2:'c', 3:'d', 4:'e' , 5:'f', 6:'g', 7:'h', 8:'i'}
     for k,v in self.mst.items():
-      print(f"{k[0]} - {k[1]}    :    {v}")
+      print(f"{alphadict[k[0]]} - {alphadict[k[1]]}    :    {v}")
 
 def main():
-  g = Graph(5)
+  g = Graph(9)
+  g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+              [4, 0, 8, 0, 0, 0, 0, 11, 0],
+              [0, 8, 0, 7, 0, 4, 0, 0, 2],
+              [0, 0, 7, 0, 9, 14, 0, 0, 0],
+              [0, 0, 0, 9, 0, 10, 0, 0, 0],
+              [0, 0, 4, 14, 10, 0, 2, 0, 0],
+              [0, 0, 0, 0, 0, 2, 0, 1, 6],
+              [8, 11, 0, 0, 0, 0, 1, 0, 7],
+              [0, 0, 2, 0, 0, 0, 6, 7, 0]]
   g.printGraph()
 
   p = Prims()
